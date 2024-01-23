afcff254715de095ee36753bf25ba72f6974d393
Ethan Netz <ethannetz@hotmail.com>
2024-01-23 18:32:01 -0500

    add brightness control

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,30 @@
 #include <Arduino.h>
-#include "BluetoothA2DPSink.h"
-
+#include <FastLED.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include "BluetoothA2DPSink.h"
 
-#include <FastLED.h>
-
-BluetoothA2DPSink a2dp_sink;
-BLECharacteristic *pHueCharacteristic;
-
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define HUE_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
-#define LED_PIN 27   // Define the pin connected to the data line of the LEDs
-#define NUM_LEDS 300 // Number of LEDs in the strip
-#define BRIGHTNESS 5 // Set brightness (0-255)
+// LED configuration
+#define LED_PIN 27
+#define NUM_LEDS 300
+#define STARTING_BRIGHTNESS 5
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
-
 CRGB leds[NUM_LEDS];
 
+// Bluetooth service and characteristic UUIDs
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define HUE_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define BRIGHTNESS_CHARACTERISTIC_UUID "04cc261b-5870-4abc-9f08-ab1ab4b90d6e"
+
+// Global variables
+BluetoothA2DPSink a2dp_sink;
+BLECharacteristic *pHueCharacteristic;
+BLECharacteristic *pBrightnessCharacteristic;
+int globalHueValue = 0;
+
+// Callbacks
 void data_received_callback()
 {
   Serial.println("Data packet received");
@@ -31,79 +35,74 @@ class MyServerCallbacks : public BLEServerCallbacks
   void onConnect(BLEServer *pServer)
   {
     Serial.println("Device connected");
-  };
+  }
 
   void onDisconnect(BLEServer *pServer)
   {
     Serial.println("Device disconnected");
-    BLEDevice::startAdvertising(); // Restart advertising after disconnection
+    BLEDevice::startAdvertising();
     Serial.println("Restarted advertising");
   }
 };
 
-int globalHueValue = 0;
 class HueCharacteristicCallbacks : public BLECharacteristicCallbacks
 {
-  // Static variable to store the timestamp of the last callback call
-  static unsigned long lastCallbackTime;
-
+public:
   void onWrite(BLECharacteristic *pCharacteristic) override
   {
-    unsigned long currentCallbackTime = micros(); // Get current time
-    unsigned long executionStartTime = currentCallbackTime;
+    // Get the value from the characteristic
+    std::string value = pCharacteristic->getValue();
 
-    // If it's not the first callback, calculate the time since the last callback
-    if (lastCallbackTime != 0)
+    if (value.length() > 0)
     {
-      unsigned long timeSinceLastCallback = currentCallbackTime - lastCallbackTime;
-      Serial.print("Time Since Last Callback (microseconds): ");
-      Serial.println(timeSinceLastCallback);
-    }
+      // Convert the first byte of the value to an int and assign to globalHueValue
+      globalHueValue = static_cast<int>(value[0]);
 
+      // Print the new hue value
+      Serial.print("New Hue Value: ");
+      Serial.println(globalHueValue);
+    }
+  }
+};
+
+class BrightnessCharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic) override
+  {
     // Get the value from the characteristic
     std::string value = pCharacteristic->getValue();
 
     if (value.length() > 0)
     {
       // Convert the first byte of the value to an int
-      globalHueValue = static_cast<int>(value[0]);
+      int brightnessValue = static_cast<int>(value[0]);
+
+      // Print the new brightness value
+      Serial.print("New Brightness Value: ");
+      Serial.println(brightnessValue);
+
+      FastLED.setBrightness(brightnessValue);
     }
-
-    // Record the execution end time
-    unsigned long executionEndTime = micros();
-    unsigned long executionTime = executionEndTime - executionStartTime;
-
-    // Print the execution time
-    Serial.print("Execution Time (microseconds): ");
-    Serial.println(executionTime);
-
-    // Update the timestamp of the last callback call
-    lastCallbackTime = executionEndTime;
   }
 };
 
-// Initialize the static variable
-unsigned long HueCharacteristicCallbacks::lastCallbackTime = 0;
-
+// Setup function
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Elephant on the loose");
 
+  // Initialize BLE Device
   BLEDevice::init("Elephant controller");
   BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks()); // Register the connection callbacks
+  pServer->setCallbacks(new MyServerCallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pHueCharacteristic = pService->createCharacteristic(
-      HUE_CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_WRITE_NR);
+      HUE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE_NR);
   pHueCharacteristic->setCallbacks(new HueCharacteristicCallbacks());
-
-  // Create another characteristic with Write No Response property
-  // BLECharacteristic *pWriteNoResponseCharacteristic = pService->createCharacteristic(
-  //     NEW_CHARACTERISTIC_UUID,
-  //     BLECharacteristic::PROPERTY_WRITE_NR);
-
+  pBrightnessCharacteristic = pService->createCharacteristic(
+      BRIGHTNESS_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE_NR);
+  pBrightnessCharacteristic->setCallbacks(new BrightnessCharacteristicCallbacks());
   pService->start();
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
@@ -128,17 +127,15 @@ void setup()
   // a2dp_sink.set_on_data_received(data_received_callback);
 
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setBrightness(STARTING_BRIGHTNESS);
 }
 
+// Main loop
 void loop()
 {
-  // Update all LEDs to the current hue value
   for (int i = 0; i < NUM_LEDS; i++)
   {
-    leds[i] = CHSV(globalHueValue, 255, 255); // Saturated and bright color
+    leds[i] = CHSV(globalHueValue, 255, 255);
   }
-
-  // Show the LEDs
   FastLED.show();
 }
